5a1b8903fa78a0e6f90b04210a471a0347b5a38b
LENOVO <LENOVO@LAPTOP-1JJ0UQG5>
2022-01-03 16:01:33 +0700

    detale page products

--- frontend/src/components/Product/MainProduct/productPape/Pape2.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Pape_2 = () => {
-  return <div>Pape_2</div>;
-};
-
-export default Pape_2;
--- frontend/src/components/Product/MainProduct/productPape/Pape3.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Pape_3 = () => {
-  return <div>Pape_3</div>;
-};
-
-export default Pape_3;
